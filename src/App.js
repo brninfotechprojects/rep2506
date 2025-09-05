85523c7f6b2ead3fc471b40f1146246535d56b65
Manjunadh <manjunadhb@gmail.com>
2025-09-05 14:57:14 +0530

    added code by mahalakshmi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>This line is added by Venkat</h1>
+      <h1>This code is added by Mahalakshmi</h1>
     </div>
   );
 }

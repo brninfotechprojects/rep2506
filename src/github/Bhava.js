49b19655ca7d28b36d418557511a3d6b3efd7c2a
Bhava Narayana <bhavanarayanachukka@gmail.com>
2025-09-06 00:30:13 +0530

    rolldice

--- src/github/Bhava.js
+++ src/github/Bhava.js
@@ -2,26 +2,39 @@ import React, {  useState } from 'react'
 
 function Bhava() {
   let [dice,setdice]=useState([]);
-  let [roll,setroll]=useState(0);
+  let [roll,setroll]=useState(null);
   let dieceroll=()=>{
     let values =[];
-    for(let i=0;i<=roll;i++){
+    for(let i=0;i<roll;i++){
       let randomnumber=Math.floor(Math.random()*6+1);
       values.push(randomnumber);
       
     }
     setdice(values)
   }
+  let incremeantroll=()=>{
+     if(roll>=0){
+      if(roll<6){
+         return dice.join(" ,")
+      }
+      else{
+        window.alert("the maximum number is reached")
+      }
+     }
+     return "not rolled"
+  }
   return (
     <div>
       <button type='button' onClick={()=>{
       dieceroll()
       }}>roll</button>
+      <br></br>
       <button type='button' onClick={()=>{
         setroll(roll+1)
         console.log(roll)
-      }}></button>
-      <p >{dice.length>0 ? dice.join(" ,"):"not rolled"}</p>
+      }} style={{position:"relative",left:"150px", bottom:"100px"}}>increment of dice  {`${roll}`}</button>
+      <br></br>
+      <p style={{position:"relative",left:"100px"}} >{incremeantroll()}</p>
     </div>
   )
 }

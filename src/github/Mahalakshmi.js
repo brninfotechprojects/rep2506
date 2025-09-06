09cd88665a0e5b2c31745d358f156963b6737e75
Mahalakshmi <mahalakshminalajala@gmail.com>
2025-09-06 16:02:16 +0530

    modified

--- src/github/Mahalakshmi.js
+++ src/github/Mahalakshmi.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 function Mahalakshmi() {
   return (
+    
     <div>
+      <nav>
+        <a className="a" href="https://en.wikipedia.org/wiki/Ganesh_Chaturthi">Ganesh</a>
+      </nav>
       <h1 className="h1">Today is saturday</h1>
       <p>It is week off</p>
+      <h1 className='heading'>Ganesh</h1>
+      <img className="ganeshPic " src="./images/ganesh.jpg"></img>
     </div>
   )
 }
